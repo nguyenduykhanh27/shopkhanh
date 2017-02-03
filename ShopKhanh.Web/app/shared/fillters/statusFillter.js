4ca42fa791da87c4991c04b208bee8a1da7faa18
duy khanh <khanh@gmail.com>
2017-02-03 06:43:56 -0800

    add filter status and format date list

---
+++ ShopKhanh.Web/app/shared/fillters/statusFillter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFillter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('khanhshop.common'));
