3d788f26350403b4926e864fd003ca0a13ce06d0
duy khanh <khanh@gmail.com>
2017-02-03 07:24:27 -0800

    paging for list

--- ShopKhanh.Web/Api/ProductCategoryController.cs
+++ ShopKhanh.Web/Api/ProductCategoryController.cs
@@ -15,20 +15,34 @@ namespace ShopKhanh.Web.Api
     [RoutePrefix("api/productcategory")]
     public class ProductCategoryController : ApiControllerBase
     {
-        IProductCategoryService _productCategoryService;
-        public ProductCategoryController(IErrorService errorService,IProductCategoryService productService) 
+        private IProductCategoryService _productCategoryService;
+
+        public ProductCategoryController(IErrorService errorService, IProductCategoryService productService)
             : base(errorService)
         {
             this._productCategoryService = productService;
         }
+
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpRespone(request, () =>
             {
+                int totalRow = 0;
+
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                totalRow = model.Count();
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
