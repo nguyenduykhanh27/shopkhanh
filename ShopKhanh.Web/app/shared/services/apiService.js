e12002cd52032b90c02499d52c1dc4f496b8d391
duy khanh <khanh@gmail.com>
2017-02-04 18:44:26 -0800

    update form and auto make SeoTile Service

--- ShopKhanh.Web/app/shared/services/apiService.js
+++ ShopKhanh.Web/app/shared/services/apiService.js
@@ -2,34 +2,51 @@
 
 (function (app) {
     app.factory('apiService', apiService);
-    apiService.$inject = ['$http','notificationService'];
+
+    apiService.$inject = ['$http', 'notificationService'];
+
     function apiService($http, notificationService) {
         return {
             get: get,
             post: post,
-
+            put : put
         }
-        function post(url,data,success,failure) {
+
+        function post(url, data, success, failure) {
             $http.post(url, data).then(function (result) {
                 success(result);
+                },function (error) {
+                    console.log(error.status)
+                    if (error.status === 401) {
+                        notificationService.displayError('Authenticate is required.');
+                    }
+                    else if (failure != null) {
+                        failure(error);
+                    }
+
+            });
+        }
+
+        function put(url, data, success, failure) {
+            $http.put(url, data).then(function (result) {
+                success(result);
             }, function (error) {
-                if (error.status === 401)
-                {
+                console.log(error.status)
+                if (error.status === 401) {
                     notificationService.displayError('Authenticate is required.');
                 }
-                else if(failure !=null){
-
+                else if (failure != null) {
                     failure(error);
                 }
-              
-            });
 
+            });
         }
+
         function get(url, params, success, failure) {
             $http.get(url, params).then(function (result) {
                 success(result);
             }, function (error) {
-                failure(Error);
+                failure(error);
             });
         }
     }
